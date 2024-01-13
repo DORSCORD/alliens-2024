058ba333dc40144ee071f975393216177d97fae7
DORSCORD <a.turchin08@gamil.com>
2024-01-13 14:18:00 +0100

    Add ship blitting

--- start.py
+++ start.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Клас для управління ресурсами та поведінкою гри"""
@@ -17,6 +18,8 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Запуск основного циклу гри"""
         while True:
@@ -25,6 +28,7 @@ class AlienInvasion:
                     sys.exit()
             
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             
             pygame.display.flip()
 
