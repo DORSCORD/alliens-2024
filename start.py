9c1046c623f9b0cca41a1c7a5f8d6ee6434a8714
DORSCORD <a.turchin08@gamil.com>
2024-01-13 13:45:27 +0100

    Add Ship image

--- start.py
+++ start.py
@@ -12,11 +12,10 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
-
-        #BackGround Color
-        self.bg_color = (230, 230, 230) #RGB
 
     def run_game(self):
         """Запуск основного циклу гри"""
@@ -25,7 +24,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             
             pygame.display.flip()
 
