5ed6a0b160e9d2536a06f085e5d07fc2c06e5fc8
DORSCORD <a.turchin08@gamil.com>
2024-01-27 12:47:41 +0100

    Add alien class and alien image

---
+++ alien.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.sprite import _Group, Sprite
+
+class Alien(Sprite):
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+
+        #Download image alien and rect
+        self.image = pygame.image.load("images/alien.bmp")
+        self.rect = self.image.get_rect()
+
+        #Every alien spawn in left-up screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        #Save gorizontale position alien
+        self.x = float(self.rect.x)
+        
